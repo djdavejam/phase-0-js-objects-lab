515c23645bf92f0642cf0c8af9f273ac7b59c7b4
David James <davidanthoneyjames@gmail.com>
2025-07-12 13:28:38 -0400

    This is the Objects Lab

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-//Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00,
+};
+
+function logAttendeeName(attendeeObj) {
+  console.log(attendeeObj.name);
+}
+
+function logTicketPrice(attendeeObj) {
+  console.log(attendeeObj.ticketPrice);
+}
+
+function updateTicketType(attendeeObj, newTicketType) {
+  attendeeObj.ticketType = newTicketType;
+}
+
+function updateTicketPrice(attendeeObj, newTicketPrice) {
+  attendeeObj.ticketPrice = newTicketPrice;
+}
+
+function removeEventProperty(attendeeObj) {
+  delete attendeeObj.event;
+}
+
+function addCheckedInProperty(attendeeObj) {
+  attendeeObj.checkedIn = true;
+}
 
 
 
-//Needed for the tests to work. Don't modify
 module.exports = {
   ...(typeof attendee !== 'undefined' && { attendee }),
   ...(typeof logAttendeeName !== 'undefined' && { logAttendeeName }),
